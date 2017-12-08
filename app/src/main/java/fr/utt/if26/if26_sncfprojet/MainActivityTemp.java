5c7917a14427b0698f2482132249b52a006dafc7
Jeanba88 <admin@jeanba88.com>
2017-12-08 16:49:29 +0100

    - Création du écran d'accueil temporaire, le temps de concevoir les fonctionnalitées.

---
+++ app/src/main/java/fr/utt/if26/if26_sncfprojet/MainActivityTemp.java
@@ -0,0 +1,31 @@
+package fr.utt.if26.if26_sncfprojet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivityTemp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_temp);
+        Button tempConfigurationButton = findViewById(R.id.temp_configuration_button);
+        tempConfigurationButton.setOnClickListener(onClickListener);
+
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.temp_configuration_button:
+                    Intent intent = new Intent(MainActivityTemp.this, Configuration.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+}
