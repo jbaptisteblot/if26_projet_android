c6fbee6e7107bd04631f265d70236c529a72411f
Jeanba88 <admin@jeanba88.com>
2017-12-08 16:24:26 +0100

    - Création de APIKeyClass permettant de récupérer la clé d'API stockée dans les SharedPreferences. - Application de la lecture et de la sauvegarde dans la page Configuration.

---
+++ app/src/main/java/fr/utt/if26/if26_sncfprojet/APIKeyClass.java
@@ -0,0 +1,26 @@
+package fr.utt.if26.if26_sncfprojet;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Permet de sauvegarder et de récupérer la clé d'API sauvegardée.
+ * Created by Jeanba on 08/12/2017.
+ */
+
+class APIKeyClass {
+    static String getKey(Context context) {
+        SharedPreferences pref = context.getSharedPreferences(
+                context.getResources().getString(R.string.apikey),
+                Context.MODE_PRIVATE);
+        return pref.getString("apikey", null);
+    }
+    static void setKey(Context context, String newKey) {
+        SharedPreferences pref = context.getSharedPreferences(
+                context.getResources().getString(R.string.apikey),
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("apikey", newKey);
+        editor.apply();
+    }
+}
--- app/src/main/java/fr/utt/if26/if26_sncfprojet/Configuration.java
+++ app/src/main/java/fr/utt/if26/if26_sncfprojet/Configuration.java
@@ -1,7 +1,12 @@
 package fr.utt.if26.if26_sncfprojet;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Configuration extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class Configuration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        final EditText configurationAPIKEYInput = findViewById(R.id.configuration_apikey_input);
+        final Button saveButton = findViewById(R.id.configuration_save_button);
+        final Context context = getApplicationContext();
+
+        String apikey = APIKeyClass.getKey(context);
+        if (apikey != null) {
+            configurationAPIKEYInput.setText(APIKeyClass.getKey(this));
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                APIKeyClass.setKey(context, configurationAPIKEYInput.getText().toString());
+                Toast.makeText(context, R.string.apikey_save_successfull, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
