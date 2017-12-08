78f46c5758dd87144c26489ce8a517c1dab268fc
Jeanba88 <admin@jeanba88.com>
2017-12-08 21:48:23 +0100

    - La recherche prend maintenant en compte ce qui est recherché.

--- app/src/main/java/fr/utt/if26/if26_sncfprojet/SearchPlace.java
+++ app/src/main/java/fr/utt/if26/if26_sncfprojet/SearchPlace.java
@@ -3,7 +3,7 @@ package fr.utt.if26.if26_sncfprojet;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,23 +14,23 @@ import org.json.JSONObject;
 
 public class SearchPlace extends AppCompatActivity {
     String apikey;
+    EditText searchPlaceInput;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_place);
 
-        Context context = getApplicationContext();
-        Button searchPlaceButton = findViewById(R.id.seachplace_button);
-        EditText searchPlaceInput = findViewById(R.id.seachplace_input);
+        context = getApplicationContext();
+        Button searchPlaceButton = findViewById(R.id.searchplace_button);
+        searchPlaceInput = findViewById(R.id.searchplace_input);
+        searchPlaceButton.setOnClickListener(onClickListener);
+        apikey = APIKeyClass.getKey(context);
 
-        String apikey = APIKeyClass.getKey(context);
-        IResult callback = volleyCallback(context);
-        VolleyService volleyService = new VolleyService(callback, context);
-        volleyService.getData("https://api.sncf.com/v1/coverage/sncf/places?q=%22Bordeaux%22", apikey);
 
     }
-    IResult volleyCallback(final Context context) {
-        IResult callback = new IResult() {
+    IResult volleyCallback() {
+        return new IResult() {
             @Override
             public void notifySuccess(JSONObject response) {
                 Toast.makeText(context, response.toString(), Toast.LENGTH_LONG).show();
@@ -40,6 +40,15 @@ public class SearchPlace extends AppCompatActivity {
                 Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
             }
         };
-        return callback;
     }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String url = "https://api.sncf.com/v1/coverage/sncf/places?q=" + searchPlaceInput.getText().toString();
+            IResult callback = volleyCallback();
+            VolleyService volleyService = new VolleyService(callback, context);
+            volleyService.getData(url, apikey);
+        }
+    };
 }
