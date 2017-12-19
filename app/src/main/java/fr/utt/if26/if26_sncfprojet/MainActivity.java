1223de62ec12456a087fc01e390e7a7ae1981702
Jeanba88 <admin@jeanba88.com>
2017-12-19 23:23:28 +0100

    - Le titre affiché change maintenant avec le ViewPager
    Passage de la toolbar dans la vue, changement dans MainActivity

--- app/src/main/java/fr/utt/if26/if26_sncfprojet/MainActivity.java
+++ app/src/main/java/fr/utt/if26/if26_sncfprojet/MainActivity.java
@@ -4,16 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager vp;
     MenuItem prevMenuItem;
+    Toolbar toolbar;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -41,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         final BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -71,6 +73,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        toolbar.setTitle("Mes trajets préférés");
+                        break;
+                    case 1:
+                        toolbar.setTitle("Mes gares préférées");
+                        break;
+                    case 2:
+                        toolbar.setTitle("Configuration");
+                        break;
+                }
                 if (prevMenuItem != null) {
                     prevMenuItem.setChecked(false);
                 }
